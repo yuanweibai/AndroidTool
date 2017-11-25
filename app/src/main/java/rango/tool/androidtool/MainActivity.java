6722b63394ddf4f852e6ffd0fa4d65a680ed464f
yuanweibai <yuanwei204@gmail.com>
2017-11-25 13:46:26 +0800

    Initial commit

---
+++ app/src/main/java/rango/tool/androidtool/MainActivity.java
@@ -0,0 +1,13 @@
+package rango.tool.androidtool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
