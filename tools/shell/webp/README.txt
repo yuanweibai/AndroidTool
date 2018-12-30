5cda9feb23bbccac1b925bf50770619513249a75
yuanweibai <yuanwei.bai@ihandysoft.com>
2018-12-30 17:54:18 +0800

    Fix...

---
+++ tools/shell/webp/README.txt
@@ -0,0 +1,9 @@
+1. cwebp_files.sh: 支持转换静态图
+2. gif2webp_files.sh: 支持转换gif图
+3. webp_all.sh: 支持转换静态图和gif图
+4. webpmux_files.sh: 支持把序列帧(无论是否是webp格式的图)转换成动态的webp图，此命令后可加一个参数，表示帧之间的时间间隔，如果不加，则默认是45ms
+
+例子：webpmux_files.sh
+1. 打开终端，切换至“....../App_ColorSMS_Android/tools/webp”目录
+2. 输入“./webpmux_files.sh” 或者 “./webpmux_files.sh 30”，并且enter
+3. 输入要转换的文件夹，enter
--- tools/shell/webp/webpmux_files.sh
+++ tools/shell/webp/webpmux_files.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+duration=45
+if [[ $1 -gt 0 ]]; then
+	duration=$1
+fi
+
 echo "Please enter file folder path"
 read fileFolderPath
 mkdir $fileFolderPath/0_webp
@@ -14,12 +19,13 @@ do
 		echo $filename
 		if [[ ${file##*.} != "webp" ]]; then
 			./bin/cwebp -q 75 $file -o $fileFolderPath/0_webp/$filename.webp
-			anim=$anim"-frame "$fileFolderPath/0_webp/$filename.webp" +40+0+0+0-b "
-		else 
-			anim=$anim"-frame "$file" +40+0+0+0-b "
+			anim=$anim"-frame "$fileFolderPath/0_webp/$filename.webp" +$duration+0+0+0-b "
+		else
+			anim=$anim"-frame "$file" +$duration+0+0+0-b "
 		fi
 	fi
 done
 mkdir $fileFolderPath/0_webp/0_anim_webp
 echo "---------webpmux command: " $anim
 ./bin/webpmux $anim -loop 0 -o $fileFolderPath/0_webp/0_anim_webp/anim.webp
+
