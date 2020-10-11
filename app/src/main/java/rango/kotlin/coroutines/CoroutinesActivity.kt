f5ccc8132d116bbc8d81d33ed383388189e502b4
yuanwei.bai <yuanwei.bai@infinigames.mobi>
2020-10-11 16:32:26 +0800

    Fix coroutines.

--- app/src/main/java/rango/kotlin/coroutines/CoroutinesActivity.kt
+++ app/src/main/java/rango/kotlin/coroutines/CoroutinesActivity.kt
@@ -14,7 +14,7 @@ class CoroutinesActivity : BaseActivity() {
         const val TAG = "CoroutinesActivity"
     }
 
-    private val coroutinesScope = CoroutineScope(Job() + Dispatchers.Main)
+    private val coroutinesScope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +27,12 @@ class CoroutinesActivity : BaseActivity() {
         parallelBtn.setOnClickListener {
             parallelRequest()
         }
+
     }
 
     private fun sequenceRequest() {
         Log.e(TAG, "requestData-Thread: " + Thread.currentThread().name)
-        coroutinesScope.launch {
+        val job = coroutinesScope.launch {
             Log.e(TAG, "launch-Thread: " + Thread.currentThread().name)
             val token = getToken()
             Log.e(TAG, "getToken end.... token = $token")
@@ -43,7 +44,7 @@ class CoroutinesActivity : BaseActivity() {
 
     private suspend fun getToken(): String {
         return withContext(Dispatchers.IO) {
-            Thread.sleep(3000)
+            delay(3000)
             Log.e(TAG, "getToken-Thread: " + Thread.currentThread().name)
             "token"
 
@@ -58,27 +59,48 @@ class CoroutinesActivity : BaseActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun parallelRequest() {
+        Log.e(TAG, "parallelRequest start....")
 
         coroutinesScope.launch {
+            Log.e(TAG, "parallel starting....")
             val imageUrl = async { getImageUrl() }
             val age = async { getAge() }
+            val address = async { getAddress() }
+
+            Log.e(TAG, "parallel image start....")
+            val url = imageUrl.await()
+            Log.e(TAG, "parallel image end.... url = $url")
+
+            Log.e(TAG, "parallel age start....")
+            val ageResult = age.await()
+            Log.e(TAG, "parallel age end.... ageResult = $ageResult")
+
+            Log.e(TAG, "parallel address start....")
+            val addressResult = address.await()
+            Log.e(TAG, "parallel address end.... address = $addressResult")
+
 
             Log.e(TAG, "parallel set text....")
-            parallelMsgText.text = "${imageUrl.await()} - ${age.await()}"
+            parallelMsgText.text = "${imageUrl.await()} - ${age.await()} - ${address.await()}"
             Log.e(TAG, "parallel set text end....")
         }
-
+        Log.e(TAG, "parallelRequest end....")
     }
 
     private suspend fun getImageUrl(): String {
         return withContext(Dispatchers.IO) {
-            Thread.sleep(5000)
+            Thread.sleep(8000)
             "imageUrl"
         }
     }
 
     private suspend fun getAge(): Int = withContext(Dispatchers.IO) {
-        Thread.sleep(9000)
+        Thread.sleep(10000)
         90
+    }
+
+    private suspend fun getAddress(): String = withContext(Dispatchers.IO) {
+        delay(15000)
+        "Address"
     }
 }
