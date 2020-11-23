a7223516dba8b12713116c3c509e082f1d255dfd
yuanwei.bai <yuanwei.bai@infinigames.mobi>
2020-11-23 11:02:50 +0800

    Add ...

--- app/src/main/java/rango/kotlin/views/BlurTextView.kt
+++ app/src/main/java/rango/kotlin/views/BlurTextView.kt
@@ -2,10 +2,8 @@ package rango.kotlin.views
 
 import android.content.Context
 import android.graphics.*
-import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.content.res.ResourcesCompat
 import rango.tool.androidtool.R
 
 class BlurTextView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null, defStyle: Int = 0) : AppCompatTextView(context, attributeSet, defStyle) {
@@ -14,34 +12,30 @@ class BlurTextView @JvmOverloads constructor(context: Context, attributeSet: Att
         const val DEFAULT_VALUE = -1111984
     }
 
-    private var blurPaint = Paint()
-
     private var gradient: LinearGradient? = null
     private var blurStartColor: Int
     private var blurEndColor: Int
-    private var blurHeight: Int
+    private var blurLineCount: Int
 
     init {
         val styleAttr = context.obtainStyledAttributes(attributeSet, R.styleable.BlurTextView)
         blurStartColor = styleAttr.getColor(R.styleable.BlurTextView_blur_start_color, DEFAULT_VALUE)
         blurEndColor = styleAttr.getColor(R.styleable.BlurTextView_blur_end_color, DEFAULT_VALUE)
-        blurHeight = styleAttr.getDimensionPixelSize(R.styleable.BlurTextView_blur_height, DEFAULT_VALUE)
+        blurLineCount = styleAttr.getInt(R.styleable.BlurTextView_blur_line_count, DEFAULT_VALUE)
         styleAttr.recycle()
     }
 
-    override fun onDrawForeground(canvas: Canvas?) {
-        super.onDrawForeground(canvas)
-
-        if (shouldDrawBlur()) {
-            if (gradient == null) {
-                gradient = LinearGradient(0f, blurHeight.toFloat(), 0f, height.toFloat(), blurStartColor, blurEndColor, Shader.TileMode.CLAMP)
-                blurPaint.shader = gradient
-            }
-            canvas?.drawRect(0f, blurHeight.toFloat(), width.toFloat(), height.toFloat(), blurPaint)
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        if (shouldDrawBlur() && gradient == null) {
+            val height = (bottom - top).toFloat()
+            val blurHeight = paint.fontSpacing * blurLineCount
+            gradient = LinearGradient(0f, height - blurHeight, 0f, height, blurStartColor, blurEndColor, Shader.TileMode.CLAMP)
+            paint.shader = gradient
         }
     }
 
     private fun shouldDrawBlur(): Boolean {
-        return blurEndColor != DEFAULT_VALUE && blurStartColor != DEFAULT_VALUE && blurHeight != DEFAULT_VALUE
+        return blurEndColor != DEFAULT_VALUE && blurStartColor != DEFAULT_VALUE && lineCount > 0
     }
 }
