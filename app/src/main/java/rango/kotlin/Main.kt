735790e73a27d258a4f207b7e09f15273bdfaaf0
yuanwei.bai <yuanwei.bai@infinigames.mobi>
2021-06-30 17:48:37 +0800

    Add ...

--- app/src/main/java/rango/kotlin/Main.kt
+++ app/src/main/java/rango/kotlin/Main.kt
@@ -10,23 +10,35 @@ fun process(test: MyTest) {
     test.run()
 }
 
-fun main(args: Array<String>) {
-    println("main")
 
-//    var x = 8
-//    process(object : MyTest() {
-//        override fun run() {
-//            if (x == 8) {
-//                print("dd")
-//                x = 89
-//            } else {
-//                print("dadfa")
-//            }
-//        }
-//    })
-//
-//    MyTest.create()
-//
-//    Diver(BaseImpl(5)).base()
+fun main(args: Array<String>) {
+    val array = intArrayOf(134, 4, 290877, 43, 9, 130, 310, 56, 1, 19)
+    println("-----------排序----------")
+
+    println("1. 冒泡排序")
+
+    var isExchange: Boolean
+    var lastExchangePosition: Int = array.size - 1
+    for (i in array.size - 1 downTo 0) {
+        isExchange = false
+        val last = if (i < lastExchangePosition) i else lastExchangePosition
+        for (k in 0 until last) {
+            if (array[k] > array[k + 1]) {
+                val temp = array[k]
+                array[k] = array[k + 1]
+                array[k + 1] = temp
+                isExchange = true
+                lastExchangePosition = k
+            }
+        }
+
+        if (!isExchange) {
+            break
+        }
+    }
+
+    array.forEach {
+        println(it)
+    }
 
 }
