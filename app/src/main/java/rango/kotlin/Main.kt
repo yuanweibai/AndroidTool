a34f3cfb8eb0bc5a29a9f93eeb00259fa7d0cf8c
yuanwei.bai <yuanwei.bai@infinigames.mobi>
2021-07-30 20:58:07 +0800

    Add ...

--- app/src/main/java/rango/kotlin/Main.kt
+++ app/src/main/java/rango/kotlin/Main.kt
@@ -4,6 +4,7 @@ import rango.kotlin.mytest.BaseImpl
 import rango.kotlin.mytest.Diver
 import rango.kotlin.mytest.MyTest
 import rango.tool.androidtool.java.HaHa
+import kotlin.math.sqrt
 
 
 fun process(test: MyTest) {
@@ -12,33 +13,46 @@ fun process(test: MyTest) {
 
 
 fun main(args: Array<String>) {
-    val array = intArrayOf(134, 4, 290877, 43, 9, 130, 310, 56, 1, 19)
-    println("-----------排序----------")
+    val array = intArrayOf(-8, 9, 0, 134, 8, 4, 290877, 8, 43, 8, 9, 130, 8, 310, 56, 1, 8, 19)
 
-    println("1. 冒泡排序")
-
-    var isExchange: Boolean
-    var lastExchangePosition: Int = array.size - 1
-    for (i in array.size - 1 downTo 0) {
-        isExchange = false
-        val last = if (i < lastExchangePosition) i else lastExchangePosition
-        for (k in 0 until last) {
-            if (array[k] > array[k + 1]) {
-                val temp = array[k]
-                array[k] = array[k + 1]
-                array[k + 1] = temp
-                isExchange = true
-                lastExchangePosition = k
-            }
-        }
-
-        if (!isExchange) {
-            break
-        }
-    }
-
+    quickly(array, 0, array.size - 1)
     array.forEach {
-        println(it)
+        println("$it")
     }
 
 }
+
+fun quickly(array: IntArray, left: Int, right: Int) {
+    if (left >= right) {
+        return
+    }
+
+    val standard = array[left]
+    var l = left
+    var r = right
+
+    while (l < r) {
+        while (array[r] >= standard && r > l) {
+            r--
+        }
+        while (array[l] <= standard && l < r) {
+            l++
+        }
+
+        if (l == r){
+            break
+        }
+
+        val temp = array[l]
+        array[l] = array[r]
+        array[r] = temp
+    }
+    // 最后的 l 与 r 必然是相等的
+    // array[l] 必然是小于 standard 的，因为在上述循环中，我们是先判断大于 standard 的，因此最后的 r 下标的值必然是小于 standard 的。
+    array[left] = array[l]
+    array[l] = standard
+    quickly(array, left, l - 1)
+    quickly(array, l + 1, right)
+}
+
+
---
+++ app/src/main/java/rango/tool/androidtool/java/Main.java
@@ -0,0 +1,29 @@
+package rango.tool.androidtool.java;
+
+public class Main {
+
+    public static class Data {
+
+        private int value;
+
+        public Data(int value) {
+            this.value = value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void changeData(Data data) {
+        data = null;
+    }
+}
